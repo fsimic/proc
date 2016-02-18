f0a11f6936e81412dd0b94774f6341a4767ddae5
fsimic <fsimic.st@gmail.com>
2016-02-18 21:36:37 +0100

    First commit

---
+++ js/services.js
@@ -0,0 +1,25 @@
+//
+//
+//
+function main() {
+
+(function () {
+   'use strict';
+
+   /* ==============================================
+  	Testimonial Slider
+  	=============================================== */ 
+
+  	$('.itpodrska').click(function() {
+        $('#service-body').html('Alooooooooo');
+      });
+
+
+}());
+
+
+
+
+
+}
+main();
